f7fb3232761a7d845e5c3e5f7f0837b91850c406
naslakboss <damiannaglak1@gmail.com>
2020-08-25 12:02:52 +0200

    clean after unsuccessfully heroku deployment

--- src/main/java/codebuddies.MealooApp/controllers/ClientController.java
+++ src/main/java/codebuddies.MealooApp/controllers/ClientController.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/client")
 public class ClientController {
