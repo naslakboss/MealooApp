fba7f47a1692dc1f2cd080a76a8322a53ccbe799
naslakboss <damiannaglak1@gmail.com>
2020-08-25 11:49:45 +0200

    second change to fix heroku problem

--- src/main/java/codebuddies.MealooApp/controllers/ClientController.java
+++ src/main/java/codebuddies.MealooApp/controllers/ClientController.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-// test change for heroku
 
 
 @RestController
