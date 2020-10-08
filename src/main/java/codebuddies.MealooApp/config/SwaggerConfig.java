f222056bc279a8add2d71db5752b5523e5da7949
naslakboss <damiannaglak1@gmail.com>
2020-10-08 09:39:47 +0200

    add configuration for swagger, allow to use it using Bearer

--- src/main/java/codebuddies.MealooApp/config/SwaggerConfig.java
+++ src/main/java/codebuddies.MealooApp/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package codebuddies.MealooApp.config;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,25 +8,58 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String DEFAULT_INCLUDE_PATTERN = "/.*";
+
     @Bean
-    public Docket api() {
+    public Docket docket() {
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .select().apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any()).build()
-                .apiInfo(apiDetails());
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .pathMapping("/")
+                .useDefaultResponseMessages(false).apiInfo(apiInfo());
     }
 
-    private ApiInfo apiDetails(){
-        return new ApiInfoBuilder().title("Mealoo App").version("1.0").build();
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
     }
 
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
 
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Mealoo App").version("1.0.0").build();
+    }
 }
+
