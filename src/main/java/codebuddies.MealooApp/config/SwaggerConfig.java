c5e509f1906c99690841f6ec3cd2f18386d11166
naslakboss <damiannaglak1@gmail.com>
2020-10-10 08:17:13 +0200

    change swagger authentication

--- src/main/java/codebuddies.MealooApp/config/SwaggerConfig.java
+++ src/main/java/codebuddies.MealooApp/config/SwaggerConfig.java
@@ -1,62 +1,43 @@
 package codebuddies.MealooApp.config;
 
-import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.List;
-
-import static springfox.documentation.builders.PathSelectors.regex;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final String DEFAULT_INCLUDE_PATTERN = "/.*";
+    @Value("$security.jwt.token")
+    private String token;
 
     @Bean
     public Docket docket() {
 
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.any())
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any()).build()
-                .securityContexts(Lists.newArrayList(securityContext()))
-                .securitySchemes(Lists.newArrayList(apiKey()))
-                .pathMapping("/")
-                .useDefaultResponseMessages(false).apiInfo(apiInfo());
-    }
-
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .forPaths(regex(DEFAULT_INCLUDE_PATTERN))
-                .build();
-    }
-
-    List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope
-                = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Lists.newArrayList(
-                new SecurityReference("JWT", authorizationScopes));
+                .useDefaultResponseMessages(false).apiInfo(apiInfo())
+                .globalOperationParameters(Collections.singletonList(
+                    new ParameterBuilder()
+                        .name("Authorization")
+                        .modelRef(new ModelRef("string"))
+                        .parameterType("header")
+                        .required(true)
+                        .hidden(true)
+                        .defaultValue("Bearer " + token)
+                        .build()));
     }
 
     private ApiInfo apiInfo() {
